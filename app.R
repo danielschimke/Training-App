7bc07bcde03bdf86ab1f21d10ad01b486e2d00af
raymond301 <raymond.moore301@gmail.com>
2020-06-08 14:16:11 -0500

    raymond adding iris starting code

--- app.R
+++ app.R
@@ -1,27 +1,69 @@
 library(shiny)
+library(shinydashboard)
+library(datasets)
+library(tidyverse)
+library(ggplot2)
 
-
-ui <- fluidPage(
-    
-    
-    titlePanel("Training"),
-    
-    sidebarLayout(
-        sidebarPanel(
-            textInput("text", "Enter text:")
-        ),
-        
-        mainPanel(
-            
-            textOutput("sample")
-        )
+########################################
+###     Graphical User Interface     ###
+########################################
+ui <- dashboardPage(
+    dashboardHeader(title = "Exploring Iris!"),
+    dashboardSidebar(
+        textInput("text", "Enter text:"),
+        textOutput("sample")
+    ),
+    dashboardBody(
+        plotOutput("petal_width"),
+        plotOutput("petal_length"),
+        plotOutput("sepal_width"),
+        plotOutput("sepal_length"),
+        tableOutput("summary_table"),
+        plotOutput("pie_by_species"),
+        plotOutput("sepal_box")
     )
 )
 
 
+#######################################
+###         Internal Server         ###
+#######################################
 server <- function(input, output) {
-    
     output$sample <- renderText(input$text)
+    
+    output$petal_width <- renderPlot({
+        ggplot(iris) +
+            geom_histogram(aes(x=Petal.Width, fill=Species))})
+    
+    output$petal_length <- renderPlot({
+        ggplot(iris) +
+            geom_histogram(aes(x=Petal.Length, fill=Species))})
+    
+    output$sepal_width <- renderPlot({
+        ggplot(iris) +
+            geom_histogram(aes(x=Sepal.Width, fill=Species))})
+    
+    output$sepal_length <- renderPlot({
+        ggplot(iris) +
+            geom_histogram(aes(x=Sepal.Length, fill=Species))})
+    
+    output$summary_table <- renderTable({
+        iris %>% 
+            group_by(Species) %>%
+            summarise(SepalLength_Mean=mean(Sepal.Length), SepalLength_StdDev=sd(Sepal.Length),
+                      SepalWidth_Mean=mean(Sepal.Width), SepalWidth_StdDev=sd(Sepal.Width),
+                      PetalLength_Mean=mean(Petal.Length), PetalLength_StdDev=sd(Petal.Length),
+                      PetalWidth_Mean=mean(Petal.Width), PetalWidth_StdDev=sd(Petal.Width) )
+    })
+    
+    output$pie_by_species <- renderPlot({
+        tbl1 <- as.data.frame(table(iris$Species)) %>% 
+            rename(Species = Var1)
+        ggplot(tbl1, aes(x = "", y = Freq, fill = Species)) +
+            geom_bar(width = 1, stat = "identity", color = "white") +
+            coord_polar("y", start = 0)+
+            theme_void()
+    })
 }
 
 
